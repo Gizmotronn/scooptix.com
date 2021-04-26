9651f01113a77873ac66440e7d7007bb7b8329d9
Paul Jeremiah <hello.paulvin@gmail.com>
2021-04-26 13:19:40 +0100

    not finish

--- lib/pages/events_overview/events_overview_page.dart
+++ lib/pages/events_overview/events_overview_page.dart
@@ -6,6 +6,7 @@ import 'package:ticketapp/UI/event_overview/event_overview_home.dart';
 import 'package:ticketapp/UI/event_overview/event_top_nav.dart';
 import 'package:ticketapp/UI/theme.dart';
 import 'package:ticketapp/model/event.dart';
+import 'package:ticketapp/pages/authentication/auth_view.dart';
 import 'package:ticketapp/pages/authentication/bloc/authentication_bloc.dart';
 import 'package:ticketapp/pages/event_details/desktop_view_drawer.dart';
 import 'package:ticketapp/pages/events_overview/bloc/events_overview_bloc.dart';
@@ -50,6 +51,10 @@ class _EventOverviewPageState extends State<EventOverviewPage> {
             child: DesktopViewDrawer(
               bloc: signUpBloc,
               linkType: null,
+              child: AuthView(
+                bloc: signUpBloc,
+                linkType: null,
+              ),
             )),
         endDrawerEnableOpenDragGesture: false,
         backgroundColor: MyTheme.appolloWhite,
