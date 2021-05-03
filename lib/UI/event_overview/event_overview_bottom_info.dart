c78a3063ea44a1a34406978d7457e0405a0f04ed
AllNamesAreTakn <alex@appollo.io>
2021-05-03 11:06:18 +0200

    Added url launchers to footer links.

--- lib/UI/event_overview/event_overview_bottom_info.dart
+++ lib/UI/event_overview/event_overview_bottom_info.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../theme.dart';
 
@@ -8,13 +9,34 @@ class EventOverviewFooter extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
-  final List<List<String>> info = [
-    ['How it works', 'Pricing', 'Sell Tickets', 'FAQ', 'Sitemap'],
-    ['Loyalty Member App', 'Scanner App', 'For Organisers', 'For large events', 'Event Management'],
-    ['Event Planning', 'Why choose appollo', 'Blog', 'Privacy Policy', 'Terms of Service'],
-    ['About', 'Help', 'Careers', 'Prices', 'Investors'],
-    ['Contact Support', 'Facebook', 'Instagram', 'Twitter', 'LinkedIn'],
-  ];
+  final Map<String, String> info = {
+    'How it works': "",
+    'Pricing': "",
+    'Sell Tickets': "",
+    'FAQ': "",
+    'Sitemap': "",
+    'Loyalty Member App': "https://appollo.io/member",
+    'Scanner App': "https://appollo.io/scanner",
+    'For Organisers': "https://appollo.io/dashboard",
+    'For large events': "",
+    'Event Management': "",
+    'Event Planning': "",
+    'Why choose appollo': "",
+    'Blog': "",
+    'Privacy Policy': "https://appollo.io/privacy-policy.html",
+    'Terms of Service': "https://appollo.io/terms-of-service.html",
+    'About': "https://appollo.io",
+    'Help': "",
+    'Careers': "",
+    'Prices': "",
+    'Investors': "",
+    'Contact Support': "contact@appollo.io",
+    'Facebook': "https://www.facebook.com/appolloapps/",
+    'Instagram': "https://www.instagram.com/appollo.io/",
+    'Twitter': "",
+    'LinkedIn': ""
+  };
+
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
@@ -26,28 +48,36 @@ class EventOverviewFooter extends StatelessWidget {
       child: Center(
         child: SizedBox(
             width: screenSize.width * 0.8,
+            height: screenSize.height * 0.3,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: AutoSizeText('Using Appollo', style: Theme.of(context).textTheme.headline6),
-                ).paddingBottom(16).paddingTop(8),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                AutoSizeText('Using Appollo', style: Theme.of(context).textTheme.headline6)
+                    .paddingBottom(16)
+                    .paddingTop(8),
+                SizedBox(
+                  height: 140,
+                  width: screenSize.width * 0.8,
+                  child: Wrap(
+                    runSpacing: MyTheme.elementSpacing,
+                    runAlignment: WrapAlignment.spaceBetween,
+                    direction: Axis.vertical,
+                    alignment: WrapAlignment.spaceBetween,
                     children: List.generate(
-                      info.length,
-                      (index) => Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: info[index]
-                            .map((text) => AutoSizeText(text,
-                                    style: Theme.of(context).textTheme.bodyText2.copyWith(fontWeight: FontWeight.w400))
-                                .paddingVertical(4))
-                            .toList(),
-                      ),
-                    ),
+                        info.length,
+                        (index) => InkWell(
+                              onTap: () async {
+                                if (await canLaunch(info[info.keys.toList()[index]])) {
+                                  await launch(info[info.keys.toList()[index]]);
+                                }
+                              },
+                              child: AutoSizeText(info.keys.toList()[index],
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2.copyWith(fontWeight: FontWeight.w400))
+                                  .paddingVertical(4),
+                            )),
                   ),
-                ),
+                ).paddingBottom(MyTheme.elementSpacing),
                 AutoSizeText(
                     '© 2021 appollo Group pty Ltd. Trademarks and brands are the property of their respective owners.',
                     textAlign: TextAlign.center,
