e03b9f5b9e6aa457f2efef7de8b98576bb03fc8f
Paul Edemekong <hello.paulvin@gmail.com>
2021-05-17 04:15:33 +0100

    adde facebook integration

---
+++ lib/services/facebook_integration.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:js/js.dart';
+import 'package:ticketapp/src/interops/facebook_interop.dart' as fb;
+
+class MessagerShare {
+  static Future<void> shareToMessager(String link, String msg) async {
+    fb.init(fb.InitOptions(appId: '2581552515272497', version: 'v7.0', cookie: true, xfbml: true));
+    fb.ui(
+      fb.LoadUI(method: "send", link: link),
+      allowInterop(
+        (jsResponse) {},
+      ),
+    );
+  }
+}
