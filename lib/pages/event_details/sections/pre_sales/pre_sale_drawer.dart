1cd45bb2afc2e8789663646311963fd5dc7bb9ab
AllNamesAreTakn <alex@appollo.io>
2021-05-21 10:23:48 +0200

    Moved pre sale prizes into separate widget

--- lib/pages/event_details/sections/pre_sales/pre_sale_drawer.dart
+++ lib/pages/event_details/sections/pre_sales/pre_sale_drawer.dart
@@ -2,11 +2,11 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:ticketapp/UI/event_details/widget/dotpoin.dart';
 import 'package:ticketapp/UI/theme.dart';
 import 'package:ticketapp/UI/widgets/buttons/apollo_button.dart';
+import 'package:ticketapp/model/pre_sale/pre_sale_prize.dart';
+import 'package:ticketapp/pages/event_details/sections/pre_sales/pre_sale_prizes_widget.dart';
 import 'package:ticketapp/repositories/user_repository.dart';
 import 'package:ticketapp/utilities/platform_detector.dart';
 import '../../../../main.dart';
@@ -22,11 +22,8 @@ class PreSaleDrawer extends StatefulWidget {
 }
 
 class _PreSaleDrawerState extends State<PreSaleDrawer> {
-  List<bool> presaleIsExpanded = [];
-
   @override
   void initState() {
-    presaleIsExpanded = List.generate(3, (index) => false);
     super.initState();
   }
 
@@ -123,45 +120,17 @@ class _PreSaleDrawerState extends State<PreSaleDrawer> {
                       AutoSizeText("Prize Pool",
                               style: MyTheme.lightTextTheme.headline4.copyWith(color: MyTheme.appolloOrange))
                           .paddingBottom(MyTheme.elementSpacing),
-                      Column(
-                        children: List.generate(
-                          presaleIsExpanded.length,
-                          (index) => PreSalePoolCard(
-                            radius: BorderRadius.only(
-                              topLeft: index == 0 ? Radius.circular(5) : Radius.zero,
-                              topRight: index == 0 ? Radius.circular(5) : Radius.zero,
-                              bottomLeft: index == presaleIsExpanded.length - 1 ? Radius.circular(5) : Radius.zero,
-                              bottomRight: index == presaleIsExpanded.length - 1 ? Radius.circular(5) : Radius.zero,
-                            ),
-                            isExpanded: presaleIsExpanded[index],
-                            ontap: () {
-                              if (!presaleIsExpanded[index]) {
-                                for (int i = 0; i < presaleIsExpanded.length; i++) {
-                                  setState(() {
-                                    presaleIsExpanded[i] = false;
-                                  });
-                                }
-                                setState(() {
-                                  presaleIsExpanded[index] = true;
-                                });
-                              } else {
-                                setState(() {
-                                  presaleIsExpanded[index] = false;
-                                });
-                              }
-                            },
-                            title: 'First Place Prize',
-                            item: [
-                              DotPoint(
-                                text: '5 Free Tickets',
-                              ),
-                              DotPoint(
-                                text: '5 Free Tickets',
-                              ),
-                            ],
-                          ),
-                        ),
-                      ).paddingBottom(MyTheme.elementSpacing),
+                      PreSalePrizesWidget(prizes: [
+                        PreSalePrize()
+                          ..name = "First Prize"
+                          ..prizes = ["5 Free Tickets", "\$50 Bar Card"],
+                        PreSalePrize()
+                          ..name = "Second Prize"
+                          ..prizes = ["3 Free Tickets", "\$30 Bar Card"],
+                        PreSalePrize()
+                          ..name = "Third Prize"
+                          ..prizes = ["1 Free Ticket", "\$10 Bar Card"]
+                      ]),
                       AutoSizeText(
                               "Winners are drawn at random once pre sale closes, and will be notified by email. Each referral point you earn rewards you with another entry, increasing your odds of winning.")
                           .paddingBottom(MyTheme.elementSpacing),
@@ -175,75 +144,6 @@ class _PreSaleDrawerState extends State<PreSaleDrawer> {
           ),
         ],
       ).paddingHorizontal(MyTheme.elementSpacing),
-    );
-  }
-}
-
-class PreSalePoolCard extends StatefulWidget {
-  final String title;
-  final List<DotPoint> item;
-  final String trailingIcon;
-  final bool isExpanded;
-  final BorderRadius radius;
-
-  final Function ontap;
-
-  const PreSalePoolCard(
-      {Key key, this.title, this.item, this.trailingIcon, this.isExpanded = false, this.radius, this.ontap})
-      : super(key: key);
-
-  @override
-  _PreSalePoolCardState createState() => _PreSalePoolCardState();
-}
-
-class _PreSalePoolCardState extends State<PreSalePoolCard> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: widget.radius,
-        color: widget.isExpanded ? MyTheme.appolloLightCardColor : MyTheme.appolloCardColorLight,
-      ),
-      child: Column(
-        children: [
-          InkWell(
-            onTap: widget.ontap,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(widget.title, style: Theme.of(context).textTheme.bodyText1.copyWith(fontWeight: FontWeight.w500)),
-                Icon(
-                  widget.isExpanded ? Icons.remove : Icons.add,
-                  size: 18,
-                  color: widget.isExpanded ? MyTheme.appolloOrange : MyTheme.appolloGreen,
-                )
-              ],
-            ).paddingTop(8).paddingBottom(4),
-          ),
-          AnimatedSwitcher(
-            duration: Duration(milliseconds: 300),
-            transitionBuilder: (child, animation) => SizeTransition(sizeFactor: animation, child: child),
-            child: Container(
-              key: ValueKey(widget.isExpanded),
-              height: widget.isExpanded ? null : 0,
-              child: Wrap(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(child: Column(children: widget.item)),
-                      widget.trailingIcon == null
-                          ? SizedBox.shrink()
-                          : SvgPicture.asset(widget.trailingIcon, height: 30),
-                    ],
-                  ).paddingTop(4).paddingBottom(4)
-                ],
-              ),
-            ),
-          )
-        ],
-      ).paddingHorizontal(8).paddingBottom(4),
     );
   }
 }
